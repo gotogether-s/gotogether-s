e8d40b5ff54096627815a1ee76f9cd7d468e953a
hyeonah <hyeonah.hello@gmail.com>
2022-09-16 21:52:19 +0900

    #8 chore: Update page to arrow function and Import reset.css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import '../styles/reset.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Component {...pageProps} />
