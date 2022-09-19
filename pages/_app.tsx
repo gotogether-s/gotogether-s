da2668dcfa6d5462022c9f226be4e85f41f3e40d
hyeonah <hyeonah.hello@gmail.com>
2022-09-19 16:01:09 +0900

    #10 design: Import global.scss and override.scss in _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import '../styles/reset.css'
+import '../styles/global.scss'
+import '../styles/override.scss'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
