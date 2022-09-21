cae8ff71bcdf5867c7047e3d29ed5a287d375ba7
SeungHun6450 <toffg6450@naver.com>
2022-09-21 13:52:45 +0900

    #16 design: Make a category

---
+++ components/Category/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import style from './Category.module.scss'
+
+function index() {
+  return (
+    <>
+      <div className={style.main}>카테고리</div>
+      <div className={style.category}>
+        <div className={style.bundle}>
+          <img width={40} height={40} src="./logo.png" alt="" />
+          <div className={style.categoryName}>전체</div>
+        </div>
+        <div className={style.bundle}>
+          <img width={40} height={40} src="./logo.png" alt="" />
+          <div className={style.categoryName}>국가별여행</div>
+        </div>
+        <div className={style.bundle}>
+          <img width={40} height={40} src="./logo.png" alt="" />
+          <div className={style.categoryName}>그룹별여행</div>
+        </div>
+        <div className={style.bundle}>
+          <img width={40} height={40} src="./logo.png" alt="" />
+          <div className={style.categoryName}>테마별여행</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default index
--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import SwiperBanner from '../SwiperBanner'
 import style from './Main.module.scss'
+import Category from '../Category'
+
 function Main() {
   return (
     <>
       <SwiperBanner />
-      <div className={style.ok}>category</div>
-      <div>recommend-product</div>
-      <div>realtime-favorite-product</div>
-      <div>nation-recommend-prodcut</div>
-      <div>group-recommend-product</div>
-      <div>theme-recommend-product</div>
+      <div className={style.container}>
+        <Category />
+        <div>recommend-product</div>
+        <div>realtime-favorite-product</div>
+        <div>nation-recommend-prodcut</div>
+        <div>group-recommend-product</div>
+        <div>theme-recommend-product</div>
+      </div>
     </>
   )
 }
