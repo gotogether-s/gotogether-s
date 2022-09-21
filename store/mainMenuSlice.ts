b1f8805b236a4c8fba1650fc872829e6ea33e4d6
hyeonah <hyeonah.hello@gmail.com>
2022-09-21 19:42:15 +0900

    #12 feat: Store main menu data in mainMenuSlice.ts and Add slice reducers to store

---
+++ store/mainMenuSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const mainMenuSlice = createSlice({
+  name: 'mainMenuSlice',
+  initialState: [
+    {
+      label: '내 정보',
+    },
+    {
+      label: '주문/예약 확인 및 취소',
+    },
+    {
+      label: '여행지 추천 받기',
+    },
+  ],
+  reducers: {},
+})
+
+export default mainMenuSlice
