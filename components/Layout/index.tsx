e4d4352f2431aa8f3c6ec28d773d705ee33ba6bd
hyeonah <hyeonah.hello@gmail.com>
2022-09-22 21:00:49 +0900

    #22 feat: Remove Nav component in Search page

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,12 +1,15 @@
 import { Container } from '@mui/system'
+import { useRouter } from 'next/router'
 import Nav from '../Nav'
 import style from './Layout.module.scss'
 
 const Layout = ({ children }: any) => {
+  const { asPath } = useRouter()
+
   return (
     <>
       <Container maxWidth="sm" className={style.container}>
-        <Nav />
+        {asPath !== 'search' && <Nav />}
         <div>{children}</div>
       </Container>
     </>
