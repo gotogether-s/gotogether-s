93a3b7e96ef539e2e6869831cde7b9e131243fed
hyeonah <hyeonah.hello@gmail.com>
2022-09-23 16:39:57 +0900

    #22 feat: Remove Nav component when router is in pageWithoutNavbar

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,15 +1,24 @@
 import { Container } from '@mui/system'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import Nav from '../Nav'
 import style from './Layout.module.scss'
 
+type ALLOWED_PATH = '/search'
+const pageWithoutNavbar: ALLOWED_PATH[] = ['/search']
+
 const Layout = ({ children }: any) => {
   const { asPath } = useRouter()
+  const [currentPath, setCurrentPath] = useState<ALLOWED_PATH | null>(null)
+
+  useEffect(() => {
+    if (asPath !== currentPath) setCurrentPath(asPath as ALLOWED_PATH)
+  }, [asPath, currentPath])
 
   return (
     <>
       <Container maxWidth="sm" className={style.container}>
-        {asPath !== '/search' && <Nav />}
+        {!pageWithoutNavbar.includes(currentPath) && <Nav />}
         <div>{children}</div>
       </Container>
     </>
