da32be65d52d20bcd51cc366570deba361a3159d
hyeonah <hyeonah.hello@gmail.com>
2022-09-23 21:01:36 +0900

    #22 feat: Create search result UI and Update style

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -3,15 +3,17 @@ import FormControl from '@mui/material/FormControl'
 import OutlinedInput from '@mui/material/OutlinedInput'
 import InputAdornment from '@mui/material/InputAdornment'
 import SearchIcon from '@mui/icons-material/Search'
-import style from './Search.module.scss'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import { add } from '/store/searchHistorySlice'
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
+import style from './Search.module.scss'
 
 const Search = () => {
   const dispatch = useDispatch()
 
   const [searchText, setSearchText] = useState('')
+  const [displaySearchResult, setDisplaySearchResult] = useState(false)
 
   const getInputValue = (e) => {
     setSearchText(e.target.value)
@@ -20,8 +22,12 @@ const Search = () => {
   const searchProduct = (e) => {
     if (e.key === 'Enter' || e.type === 'click') {
       dispatch(add(searchText))
-      setSearchText('')
+      setDisplaySearchResult(true)
     }
+  }
+
+  const clearInput = () => {
+    setSearchText('')
   }
 
   return (
@@ -35,17 +41,30 @@ const Search = () => {
             onChange={getInputValue}
             onKeyDown={searchProduct}
             endAdornment={
-              <InputAdornment position="end" onClick={searchProduct}>
-                <SearchIcon
-                  sx={{ fontSize: 25 }}
-                  className={style['search-icon']}
-                />
+              <InputAdornment position="end">
+                {!displaySearchResult ? (
+                  <SearchIcon
+                    sx={{ fontSize: 25 }}
+                    className={style['clickable-icon']}
+                    onClick={searchProduct}
+                  />
+                ) : (
+                  <HighlightOffIcon
+                    sx={{ fontSize: 25 }}
+                    className={style['clickable-icon']}
+                    onClick={clearInput}
+                  />
+                )}
               </InputAdornment>
             }
           />
         </FormControl>
       </div>
-      <div className={style['recent-search']}>최근 검색어</div>
+      {!displaySearchResult ? (
+        <div className={style['label']}>최근 검색어</div>
+      ) : (
+        <div className={style['label']}>상품 검색결과</div>
+      )}
     </>
   )
 }
