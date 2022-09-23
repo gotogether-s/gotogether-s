2c4f95ad6ab89857087cfc74f3834aeb76dc49d5
hyeonah <hyeonah.hello@gmail.com>
2022-09-23 16:58:13 +0900

    #22 feat: Create theme to change MUI default font size

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store from '../store'
@@ -7,13 +8,21 @@ import '../styles/swiperOverride.scss'
 import '../styles/global.scss'
 import '../styles/override.scss'
 
+const theme = createTheme({
+  typography: {
+    htmlFontSize: 10,
+  },
+})
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </ThemeProvider>
   )
 }
 
