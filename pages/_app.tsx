6f959b9708c4834dbad7f39a63cc9943ac5e6484
hyeonah <hyeonah.hello@gmail.com>
2022-09-23 21:37:19 +0900

    #22 chore: Update path url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 import { ThemeProvider } from '@mui/material/styles'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
-import store from '../store'
-import Layout from '../components/Layout'
-import '../styles/reset.css'
-import '../styles/swiperOverride.scss'
-import '../styles/global.scss'
-import '../styles/muiOverride.scss'
-import theme from '../styles/theme'
+import store from '/store'
+import Layout from '/components/Layout'
+import '/styles/reset.css'
+import '/styles/swiperOverride.scss'
+import '/styles/global.scss'
+import '/styles/muiOverride.scss'
+import theme from '/styles/theme'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
