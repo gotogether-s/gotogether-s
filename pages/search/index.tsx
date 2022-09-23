3c64a904fb7872a92a68207c83592c0798f2ef63
hyeonah <hyeonah.hello@gmail.com>
2022-09-23 20:15:49 +0900

    #22 feat: Store search data in slice after pressing enter key or clicking button

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -4,8 +4,26 @@ import OutlinedInput from '@mui/material/OutlinedInput'
 import InputAdornment from '@mui/material/InputAdornment'
 import SearchIcon from '@mui/icons-material/Search'
 import style from './Search.module.scss'
+import { add } from '/store/searchHistorySlice'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
 
 const Search = () => {
+  const dispatch = useDispatch()
+
+  const [searchText, setSearchText] = useState('')
+
+  const getInputValue = (e) => {
+    setSearchText(e.target.value)
+  }
+
+  const searchProduct = (e) => {
+    if (e.key === 'Enter' || e.type === 'click') {
+      dispatch(add(searchText))
+      setSearchText('')
+    }
+  }
+
   return (
     <>
       <div className={style['search-container']}>
@@ -13,9 +31,15 @@ const Search = () => {
         <FormControl sx={{ width: '100%' }} size="small">
           <OutlinedInput
             placeholder="상품을 검색해주세요"
+            value={searchText}
+            onChange={getInputValue}
+            onKeyDown={searchProduct}
             endAdornment={
-              <InputAdornment position="end">
-                <SearchIcon sx={{ fontSize: 25 }} />
+              <InputAdornment position="end" onClick={searchProduct}>
+                <SearchIcon
+                  sx={{ fontSize: 25 }}
+                  className={style['search-icon']}
+                />
               </InputAdornment>
             }
           />
