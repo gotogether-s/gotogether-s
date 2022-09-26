feb72395527a5f44fb72e188dfc1fb28ee6b99ce
hyeonah <hyeonah.hello@gmail.com>
2022-09-26 22:47:46 +0900

    #37 feat: Create basic sign up UI

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -4,6 +4,7 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker'
 import TextField from '@mui/material/TextField'
 import { useState } from 'react'
+import Button from '@mui/material/Button'
 import NavBar from '../../components/NavBar'
 import style from './SignUp.module.scss'
 
@@ -30,6 +31,38 @@ const SignUp = () => {
               )}
             />
           </LocalizationProvider>
+        </div>
+        <div className={style['input-wrapper']}>
+          <div className={style['label']}>이메일</div>
+          <TextField
+            name="email"
+            size="small"
+            placeholder="이메일을 입력해주세요"
+            sx={{ width: '100%' }}
+          />
+        </div>
+        <div className={style['input-wrapper']}>
+          <div className={style['label']}>비밀번호</div>
+          <TextField
+            name="password"
+            size="small"
+            placeholder="비밀번호를 입력해주세요"
+            sx={{ width: '100%' }}
+          />
+        </div>
+        <div className={style['input-wrapper']}>
+          <div className={style['label']}>비밀번호 확인</div>
+          <TextField
+            name="password"
+            size="small"
+            placeholder="비밀번호를 다시 입력해주세요"
+            sx={{ width: '100%' }}
+          />
+        </div>
+        <div className={style['button-wrapper']}>
+          <Button variant="contained" type="submit">
+            회원가입
+          </Button>
         </div>
       </form>
     </>
