ffa28ac272c5ab6efdfd6cc0eaf612e9e9cd6d30
hyeonah <hyeonah.hello@gmail.com>
2022-09-26 18:54:21 +0900

    #34 feat: Import NavBar in SignIn page

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,33 +1,37 @@
 import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import style from './SignIn.module.scss'
+import NavBar from '../../components/NavBar'
 
 const SignIn = () => {
   return (
-    <form>
-      <div className={style['input-wrapper']}>
-        <div className={style['label']}>이메일</div>
-        <TextField
-          placeholder="이메일을 입력해주세요"
-          size="small"
-          sx={{ width: '100%' }}
-        />
-      </div>
-      <div className={style['input-wrapper']}>
-        <div className={style['label']}>비밀번호</div>
-        <TextField
-          placeholder="비밀번호를 입력해주세요"
-          size="small"
-          sx={{ width: '100%' }}
-        />
-      </div>
-      <div className={style['button-wrapper']}>
-        <Button variant="contained" type="submit">
-          로그인
-        </Button>
-      </div>
-      <div className={style['signup-text']}>회원가입</div>
-    </form>
+    <>
+      <NavBar link="/" title="로그인" />
+      <form>
+        <div className={style['input-wrapper']}>
+          <div className={style['label']}>이메일</div>
+          <TextField
+            placeholder="이메일을 입력해주세요"
+            size="small"
+            sx={{ width: '100%' }}
+          />
+        </div>
+        <div className={style['input-wrapper']}>
+          <div className={style['label']}>비밀번호</div>
+          <TextField
+            placeholder="비밀번호를 입력해주세요"
+            size="small"
+            sx={{ width: '100%' }}
+          />
+        </div>
+        <div className={style['button-wrapper']}>
+          <Button variant="contained" type="submit">
+            로그인
+          </Button>
+        </div>
+        <div className={style['signup-text']}>회원가입</div>
+      </form>
+    </>
   )
 }
 
