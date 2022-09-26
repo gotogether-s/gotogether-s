2aa386b635d9869b5400e828e55b675b83820182
hyeonah <hyeonah.hello@gmail.com>
2022-09-26 19:06:12 +0900

    #34 feat: Get email and password input values when it's onChange

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -2,8 +2,20 @@ import TextField from '@mui/material/TextField'
 import Button from '@mui/material/Button'
 import style from './SignIn.module.scss'
 import NavBar from '../../components/NavBar'
+import { useState } from 'react'
 
 const SignIn = () => {
+  const initialValue = {
+    email: '',
+    password: '',
+  }
+  const [formValues, setFormValues] = useState(initialValue)
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+
   return (
     <>
       <NavBar link="/" title="로그인" />
@@ -11,17 +23,23 @@ const SignIn = () => {
         <div className={style['input-wrapper']}>
           <div className={style['label']}>이메일</div>
           <TextField
-            placeholder="이메일을 입력해주세요"
+            name="email"
             size="small"
+            placeholder="이메일을 입력해주세요"
+            value={formValues.email}
             sx={{ width: '100%' }}
+            onChange={handleInputChange}
           />
         </div>
         <div className={style['input-wrapper']}>
           <div className={style['label']}>비밀번호</div>
           <TextField
-            placeholder="비밀번호를 입력해주세요"
+            name="password"
             size="small"
+            placeholder="비밀번호를 입력해주세요"
+            value={formValues.password}
             sx={{ width: '100%' }}
+            onChange={handleInputChange}
           />
         </div>
         <div className={style['button-wrapper']}>
