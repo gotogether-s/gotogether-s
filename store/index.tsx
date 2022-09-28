e7c562401a737127f72caf1040f317a6fb6d4db3
SeungHun6450 <toffg6450@naver.com>
2022-09-28 22:05:21 +0900

    #30 feat: temporary infomation use redux, detail to reservation page

--- store/index.tsx
+++ store/index.tsx
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import mainMenuSlice from './mainMenuSlice'
 import categoryMenuSlice from './categoryMenuSlice'
 import searchHistorySlice from './searchHistorySlice'
+import detailToReservationSlice from './detailToReservationSlice'
 
 const store = configureStore({
   reducer: {
     mainMenu: mainMenuSlice.reducer,
     categoryMenu: categoryMenuSlice.reducer,
     searchHistory: searchHistorySlice.reducer,
+    detailToReservation: detailToReservationSlice.reducer,
   },
 })
 
