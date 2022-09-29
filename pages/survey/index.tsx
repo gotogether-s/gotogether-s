8146a95ee5ca1f41b4882b34828cf6c1fb614fcd
hyeonah <hyeonah.hello@gmail.com>
2022-09-29 21:33:49 +0900

    #46 feat: Import QnA component in survey page and Add margin bottom value

--- pages/survey/index.tsx
+++ pages/survey/index.tsx
@@ -1,5 +1,6 @@
-import style from './Survey.module.scss'
 import NavBar from '/components/NavBar'
+import QnA from '/components/QnA'
+import style from './Survey.module.scss'
 
 const Survey = () => {
   return (
@@ -9,6 +10,7 @@ const Survey = () => {
         <h1>나에게 꼭 맞는 여행지 추천을 위해 항목을 선택해주세요</h1>
         <p>선택한 항목은 마이페이지에서 변경할 수 있습니다.</p>
       </div>
+      <QnA />
     </>
   )
 }
