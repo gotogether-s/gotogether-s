e4c9e01a3b3cbd9fa0d07f6a2d411537df1506e5
hyeonah <hyeonah.hello@gmail.com>
2022-09-29 20:13:09 +0900

    #46 feat: Create NavBar and intro section

--- pages/survey/index.tsx
+++ pages/survey/index.tsx
@@ -1,7 +1,16 @@
 import style from './Survey.module.scss'
+import NavBar from '/components/NavBar'
 
 const Survey = () => {
-  return <div>Survey</div>
+  return (
+    <>
+      <NavBar link="/" />
+      <div className={style['intro']}>
+        <h1>나에게 꼭 맞는 여행지 추천을 위해 항목을 선택해주세요</h1>
+        <p>선택한 항목은 마이페이지에서 변경할 수 있습니다.</p>
+      </div>
+    </>
+  )
 }
 
 export default Survey
