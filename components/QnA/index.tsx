a27ae9a94f80004891b0e34b631bacdb0c26139c
hyeonah <hyeonah.hello@gmail.com>
2022-09-30 15:05:47 +0900

    #46 feat: Get user answer onClick and Save data to state

--- components/QnA/index.tsx
+++ components/QnA/index.tsx
@@ -3,12 +3,34 @@ import List from '@mui/material/List'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 import style from './QnA.module.scss'
 
 const QnA = () => {
+  const [userSurveyResult, setUserSurveyResult] = useState({
+    ages: '',
+    gender: '',
+    companion: '',
+    religion: '',
+    theme: '',
+  })
+
   const qnaLists = useSelector((state) => {
     return state.surveyQnaLists
   })
+
+  const getUserAnswer = (e, qnaListindex) => {
+    const key = Object.keys(userSurveyResult)[qnaListindex]
+    const value = e.target.textContent
+    setUserSurveyResult({
+      ...userSurveyResult,
+      [key]: value,
+    })
+  }
+
+  useEffect(() => {
+    console.log(userSurveyResult)
+  }, [userSurveyResult])
 
   return (
     <>
@@ -21,7 +43,10 @@ const QnA = () => {
           <Box className={style['answer-wrapper']}>
             <List sx={{ padding: 0 }}>
               {qnaList.answers.map((answer, answerIndex) => (
-                <ListItemButton key={answerIndex}>
+                <ListItemButton
+                  key={answerIndex}
+                  onClick={() => getUserAnswer(event, qnaListindex)}
+                >
                   <ListItemText primary={answer} sx={{ textAlign: 'center' }} />
                 </ListItemButton>
               ))}
