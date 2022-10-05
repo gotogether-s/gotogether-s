ca1bd1e0fbc9178c1fb9e81f0a3afc44a76f9805
SeungHun6450 <toffg6450@naver.com>
2022-10-05 21:31:13 +0900

    #30 feat: Add ShareLink folders and Edit Summary

---
+++ components/ProductDetail/ShareLink/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from './ShareLink.module.scss'
+
+function index() {
+  return (
+    <>
+      <div className={style.share}>공유하기</div>
+    </>
+  )
+}
+
+export default index
