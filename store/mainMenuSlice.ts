bcb6c5271e85d0ff693ee678bc8237d8eb3ecf29
hyeonah <hyeonah.hello@gmail.com>
2022-10-13 18:55:10 +0900

    #24 chore: Add link value to mainMenuSlice

--- store/mainMenuSlice.ts
+++ store/mainMenuSlice.ts
@@ -9,12 +9,15 @@ const mainMenuSlice: mainMenuState = createSlice({
   initialState: [
     {
       label: '내 정보',
+      link: '/myinfo',
     },
     {
       label: '주문/예약 확인 및 취소',
+      link: '/mybooking',
     },
     {
       label: '여행지 추천 받기',
+      link: '/survey',
     },
   ],
   reducers: {},
