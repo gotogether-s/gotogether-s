9cd6e249ba1892797a8a76b828b0798157cf1c0a
hyeonah <hyeonah.hello@gmail.com>
2022-11-11 20:22:58 +0900

    #109 feat: Open and Close sideBar with slice and Update transitionDuration

--- components/SideBar/Feature/Menu/index.tsx
+++ components/SideBar/Feature/Menu/index.tsx
@@ -1,6 +1,7 @@
 import { List, ListItem, ListItemButton, ListItemText } from '@mui/material'
 import Link from 'next/link'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { close } from '@store/sideBarStatusSlice'
 import Category from './Category'
 import style from './Menu.module.scss'
 
@@ -8,9 +9,10 @@ const Menu = () => {
   const mainMenus = useSelector((state) => {
     return state.mainMenu
   })
+  const dispatch = useDispatch()
 
   return (
-    <List>
+    <List onClick={() => dispatch(close())}>
       {mainMenus.map((mainMenu: any, index: number) => (
         <Link key={index} href={mainMenu.link}>
           <ListItem disablePadding>
