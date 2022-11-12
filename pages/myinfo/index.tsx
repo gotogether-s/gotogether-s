9ce9b432ae4f9498d1ab21c32705a24b21eb353c
hyeonah <hyeonah.hello@gmail.com>
2022-11-12 21:44:06 +0900

    #114 feat: Request user info and Display name, birthday, email

--- pages/myinfo/index.tsx
+++ pages/myinfo/index.tsx
@@ -1,7 +1,41 @@
+import { useMembersDetailMutation } from '@api/requestApi'
+import { useState, useEffect } from 'react'
 import style from './MyInfo.module.scss'
 
 const MyInfo = () => {
-  return <>마이페이지는 준비중입니다 😅</>
+  const [membersDetail] = useMembersDetailMutation()
+
+  const [userName, setUserName] = useState('')
+  const [userBirthday, setUserBirthday] = useState('')
+  const [userEmail, setUserEmail] = useState('')
+
+  useEffect(() => {
+    requestUserInfo()
+  }, [])
+
+  const requestUserInfo = async () => {
+    try {
+      const accessToken = localStorage.getItem('accessToken')
+      const res = await membersDetail({
+        accessToken: accessToken,
+      })
+      console.log('res: ', res)
+      const { name, birth, email } = res.data.data
+      setUserName(name)
+      setUserBirthday(birth)
+      setUserEmail(email)
+    } catch (e) {
+      console.log('e: ', e)
+    }
+  }
+
+  return (
+    <>
+      <p>{userName}</p>
+      <p>{userBirthday}</p>
+      <p>{userEmail}</p>
+    </>
+  )
 }
 
 export default MyInfo
