8320e20d5ada25d00d6a42f9abb297217d0c6d68
hyeonah <hyeonah.hello@gmail.com>
2022-11-13 21:57:29 +0900

    #114 feat: Create new password and confirm input

--- pages/newpassword/index.tsx
+++ pages/newpassword/index.tsx
@@ -1,7 +1,30 @@
+import { TextField } from '@mui/material'
 import style from './NewPassword.module.scss'
 
 const NewPassword = () => {
-  return <>NewPassword</>
+  return (
+    <>
+      <p>새로운 비밀번호를 입력해주세요.</p>
+      <div className={style['input-wrapper']}>
+        <div className={style['label']}>새로운 비밀번호</div>
+        <TextField
+          name="password"
+          size="small"
+          placeholder="새로운 비밀번호를 입력해주세요"
+          sx={{ width: '100%' }}
+        />
+      </div>
+      <div className={style['input-wrapper']}>
+        <div className={style['label']}>새로운 비밀번호 확인</div>
+        <TextField
+          name="passwordConfirm"
+          size="small"
+          placeholder="새로운 비밀번호를 다시 입력해주세요"
+          sx={{ width: '100%' }}
+        />
+      </div>
+    </>
+  )
 }
 
 export default NewPassword
