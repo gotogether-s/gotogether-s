087fb53b26a4dcb84e2d2b00b7b4d009421b33af
hyeonah <hyeonah.hello@gmail.com>
2022-11-13 21:48:45 +0900

    #114 feat: Display responseMessage and Add style

--- pages/confirmpassword/index.tsx
+++ pages/confirmpassword/index.tsx
@@ -1,6 +1,6 @@
 import { TextField, Button } from '@mui/material'
 import { useConfirmPasswordMutation } from '@api/requestApi'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import style from './ConfirmPassword.module.scss'
 
 const ConfirmPassword = () => {
@@ -8,6 +8,8 @@ const ConfirmPassword = () => {
 
   const [passwordValue, setPasswordValue] = useState('')
   const [passwordValueError, setPasswordValueError] = useState({})
+  const [passwordConfirmResponseMessage, setPasswordConfirmResponseMessage] =
+    useState('')
 
   const handlePasswordValueChange = (e) => {
     const { value } = e.target
@@ -17,6 +19,7 @@ const ConfirmPassword = () => {
   const goToNext = async () => {
     const passwordValidation = validatePassword(passwordValue)
     setPasswordValueError(validatePassword(passwordValue))
+    setPasswordConfirmResponseMessage('')
 
     if (Object.keys(passwordValidation).length !== 0) return
 
@@ -30,8 +33,14 @@ const ConfirmPassword = () => {
         accessToken: accessToken,
       })
       console.log('res: ', res)
+      if (res.data.statusCode === 200) {
+        setPasswordConfirmResponseMessage('비밀번호가 일치합니다!')
+      } else if (res.data.statusCode === 400) {
+        setPasswordConfirmResponseMessage('비밀번호가 일치하지 않습니다!')
+      }
     } catch (e) {
       console.log('e: ', e)
+      setPasswordConfirmResponseMessage('비밀번호가 일치하지 않습니다!')
     }
   }
 
@@ -67,6 +76,19 @@ const ConfirmPassword = () => {
       <Button onClick={goToNext} variant="contained" sx={{ width: '100%' }}>
         다음
       </Button>
+      <p
+        className={
+          passwordConfirmResponseMessage !== '비밀번호가 일치합니다!'
+            ? style['error-message']
+            : style['success-message']
+        }
+        style={{
+          visibility:
+            passwordConfirmResponseMessage !== '' ? 'visible' : 'hidden',
+        }}
+      >
+        {passwordConfirmResponseMessage}
+      </p>
     </>
   )
 }
