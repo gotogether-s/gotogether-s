cddd8283f83321471bb9f05c78aa9e13b6989d9d
hyeonah <hyeonah.hello@gmail.com>
2022-11-13 19:37:50 +0900

    #114 feat: Create password input and Update value onChange

--- pages/confirmpassword/index.tsx
+++ pages/confirmpassword/index.tsx
@@ -1,7 +1,28 @@
+import { TextField } from '@mui/material'
+import { useState } from 'react'
 import style from './ConfirmPassword.module.scss'
 
 const ConfirmPassword = () => {
-  return <>ConfirmPassword</>
+  const [passwordValue, setPasswordValue] = useState('')
+
+  const handlePasswordValueChange = (e) => {
+    const { value } = e.target
+    setPasswordValue(value)
+  }
+
+  return (
+    <div className={style['input-wrapper']}>
+      <div className={style['label']}>기존 비밀번호를 입력해주세요.</div>
+      <TextField
+        name="password"
+        size="small"
+        placeholder="기존 비밀번호 입력"
+        sx={{ width: '100%' }}
+        value={passwordValue}
+        onChange={handlePasswordValueChange}
+      />
+    </div>
+  )
 }
 
 export default ConfirmPassword
