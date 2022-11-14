24938e84fbc62bb791c6c0797a24419e28a99101
hyeonah <hyeonah.hello@gmail.com>
2022-11-14 18:50:26 +0900

    #114 feat: Display error and response message for password change request

--- pages/newpassword/index.tsx
+++ pages/newpassword/index.tsx
@@ -10,6 +10,9 @@ const NewPassword = () => {
     password: '',
     passwordConfirm: '',
   })
+  const [passwordValuesErrors, setPasswordValuesErrors] = useState({})
+  const [passwordUpdateResponseMessage, setPasswordUpdateResponseMessage] =
+    useState('')
 
   const handleNewPasswordValuesChange = (e) => {
     const { name, value } = e.target
@@ -17,6 +20,12 @@ const NewPassword = () => {
   }
 
   const requestPasswordChange = async () => {
+    const passwordValidation = validatePassword(newPasswordValues)
+    setPasswordValuesErrors(validatePassword(newPasswordValues))
+    setPasswordUpdateResponseMessage('')
+
+    if (Object.keys(passwordValidation).length !== 0) return
+
     try {
       const accessToken = localStorage.getItem('accessToken')
       const password = {
@@ -27,9 +36,37 @@ const NewPassword = () => {
         accessToken: accessToken,
       })
       console.log('res: ', res)
+      if (res.data.statusCode === 200) {
+        setPasswordUpdateResponseMessage('비밀번호 변경에 성공했습니다!')
+      } else {
+        setPasswordUpdateResponseMessage(
+          '비밀번호 변경에 실패했습니다! 다시 시도해주세요!',
+        )
+      }
     } catch (e) {
       console.log('e: ', e)
+      setPasswordUpdateResponseMessage(
+        '비밀번호 변경에 실패했습니다! 다시 시도해주세요!',
+      )
     }
+  }
+
+  const validatePassword = (values) => {
+    const errors = {}
+    if (!values.password) {
+      errors.password = '비밀번호를 입력해주세요!'
+    }
+    if (!values.passwordConfirm) {
+      errors.passwordConfirm = '비밀번호를 다시 입력해주세요!'
+    }
+    if (
+      values.password &&
+      values.passwordConfirm &&
+      values.password !== values.passwordConfirm
+    ) {
+      errors.passwordConfirm = '비밀번호가 일치하지 않습니다!'
+    }
+    return errors
   }
 
   return (
@@ -44,7 +81,15 @@ const NewPassword = () => {
           value={newPasswordValues.password}
           sx={{ width: '100%' }}
           onChange={handleNewPasswordValuesChange}
-        />
+        />{' '}
+        <p
+          style={{
+            visibility: passwordValuesErrors.password ? 'visible' : 'hidden',
+          }}
+          className={style['error-message']}
+        >
+          {passwordValuesErrors.password}
+        </p>
       </div>
       <div className={style['input-wrapper']}>
         <div className={style['label']}>새로운 비밀번호 확인</div>
@@ -56,6 +101,16 @@ const NewPassword = () => {
           sx={{ width: '100%' }}
           onChange={handleNewPasswordValuesChange}
         />
+        <p
+          style={{
+            visibility: passwordValuesErrors.passwordConfirm
+              ? 'visible'
+              : 'hidden',
+          }}
+          className={style['error-message']}
+        >
+          {passwordValuesErrors.passwordConfirm}
+        </p>
       </div>
       <Button
         onClick={requestPasswordChange}
@@ -64,6 +119,19 @@ const NewPassword = () => {
       >
         확인
       </Button>
+      <p
+        className={
+          passwordUpdateResponseMessage !== '비밀번호 변경에 성공했습니다!'
+            ? style['error-message']
+            : style['success-message']
+        }
+        style={{
+          visibility:
+            passwordUpdateResponseMessage !== '' ? 'visible' : 'hidden',
+        }}
+      >
+        {passwordUpdateResponseMessage}
+      </p>
     </>
   )
 }
