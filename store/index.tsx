e6d60920a2eb1ab2d87ac72af9243bac6e4ea874
hyeonah <hyeonah.hello@gmail.com>
2022-11-21 16:13:44 +0900

    #118 chore: Rename alias from slice

--- store/index.tsx
+++ store/index.tsx
@@ -1,14 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { requestApi } from '@api/requestApi'
-import categoryMenuSlice from './categoryMenuSlice'
-import searchHistorySlice from './searchHistorySlice'
+import categoryMenu from './categoryMenuSlice'
+import searchHistory from './searchHistorySlice'
 import surveyQnaLists from './surveyQnaListsSlice'
 import sideBarStatus from './sideBarStatusSlice'
 
 const store = configureStore({
   reducer: {
-    categoryMenu: categoryMenuSlice.reducer,
-    searchHistory: searchHistorySlice.reducer,
+    categoryMenu: categoryMenu.reducer,
+    searchHistory: searchHistory.reducer,
     surveyQnaLists: surveyQnaLists.reducer,
     sideBarStatus: sideBarStatus.reducer,
     [requestApi.reducerPath]: requestApi.reducer,
