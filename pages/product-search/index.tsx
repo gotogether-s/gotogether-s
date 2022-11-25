b207c868403b8616b8a358b3814a6f32f92f77bf
hyeonah <hyeonah.hello@gmail.com>
2022-11-25 16:41:24 +0900

    #119 feat: Display productLists after search

--- pages/product-search/index.tsx
+++ pages/product-search/index.tsx
@@ -3,6 +3,7 @@ import SearchIcon from '@mui/icons-material/Search'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import AccessTimeIcon from '@mui/icons-material/AccessTime'
 import CloseIcon from '@mui/icons-material/Close'
+import Link from 'next/link'
 import { useSearchProductsMutation } from '@api/requestApi'
 import { FormControl, OutlinedInput, InputAdornment } from '@mui/material'
 import { useRouter } from 'next/router'
@@ -23,6 +24,8 @@ const ProductSearch = () => {
 
   const [keyword, setKeyword] = useState('')
   const [displaySearchResult, setDisplaySearchResult] = useState(false)
+  const [productNumber, setProductNumber] = useState(0)
+  const [productLists, setProductLists] = useState(null)
 
   const getInputValue = (e) => {
     setKeyword(e.target.value)
@@ -50,6 +53,12 @@ const ProductSearch = () => {
       const res = await searchProducts(keyword)
       console.log('res: ', res)
       router.push(`/product-search?keyword=${keyword}&page=0`)
+      const { content } = res.data.data
+      console.log(content)
+      if (content.length) {
+        setProductNumber(content.length)
+        setProductLists(content)
+      }
     } catch (e) {
       console.log('e: ', e)
     }
@@ -109,8 +118,33 @@ const ProductSearch = () => {
         </>
       ) : (
         <>
-          <div className={style['label']}>상품 검색결과 0</div>
-          <p className={style['no-result']}>상품 검색 결과가 없습니다.</p>
+          <div className={style['label']}>검색결과 {productNumber}</div>
+          {productNumber ? (
+            <div className="productLists">
+              {productLists.map(({ ...list }, index) => (
+                <div className="productList" key={index}>
+                  <Link href={`/product-details/${list.id}`}>
+                    <img src={list.thumbnail} alt="img" className="imgClick" />
+                  </Link>
+                  <span className="nation">{list.country}</span>
+                  <div className="title">{list.productName}</div>
+                  <div className="hashTags">
+                    <div className="hashTag1">#{list.ages} &nbsp;</div>
+                    <div className="hashTag2">#{list.theme} &nbsp;</div>
+                  </div>
+                  {list.basicPrice == 0 ? (
+                    <div className="price">가격 문의</div>
+                  ) : (
+                    <div className="price">
+                      {list.basicPrice.toLocaleString('ko-KR')}원
+                    </div>
+                  )}
+                </div>
+              ))}
+            </div>
+          ) : (
+            <p className={style['no-result']}>상품 검색 결과가 없습니다.</p>
+          )}
         </>
       )}
     </>
