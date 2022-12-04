e3ce9523e36b91c90ad9a2179f6fa1735c97dbb6
hyeonah <hyeonah.hello@gmail.com>
2022-12-04 18:13:46 +0900

    #144 feat: Duplicate BookingClientInfo when checkbox is checked

--- components/TravellerInfoForm/index.tsx
+++ components/TravellerInfoForm/index.tsx
@@ -8,10 +8,35 @@ import {
   TextField,
   Typography,
 } from '@mui/material'
+import { useSelector } from 'react-redux'
+import { useState, useEffect } from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import style from './TravellerInfoForm.module.scss'
 
 const TravellerInfoForm = ({ number }) => {
+  const [duplicateClientInfo, setDuplicateClientInfo] = useState(false)
+
+  const updateDuplicateClientInfoState = () => {
+    setDuplicateClientInfo(!duplicateClientInfo)
+  }
+
+  const getBookingClientInfo = useSelector((state) => {
+    return state.bookingClientInfo
+  })
+
+  const getClientInfo = () => {
+    const { name, phone } = getBookingClientInfo
+    if (name === '' || phone === '') {
+      console.log('예약자 정보를 먼저 입력해주세요')
+    } else {
+      console.log('예약자 정보 복사 후 입력')
+    }
+  }
+
+  useEffect(() => {
+    duplicateClientInfo && getClientInfo()
+  }, [duplicateClientInfo])
+
   return (
     <Accordion
       defaultExpanded={true}
@@ -42,7 +67,12 @@ const TravellerInfoForm = ({ number }) => {
             }}
           >
             <FormControlLabel
-              control={<Checkbox />}
+              control={
+                <Checkbox
+                  checked={duplicateClientInfo}
+                  onChange={updateDuplicateClientInfoState}
+                />
+              }
               label="예약자 정보와 동일"
               sx={{
                 margin: '0 0 1rem 0',
