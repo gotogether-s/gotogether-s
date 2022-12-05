132e317501fb53fd622c00b021c4466c6b80db6f
hyeonah <hyeonah.hello@gmail.com>
2022-12-05 13:39:34 +0900

    #144 feat: Display ModalWindow when checkbox is checked and clientInfo is empty

--- components/TravellerInfoForm/index.tsx
+++ components/TravellerInfoForm/index.tsx
@@ -10,8 +10,10 @@ import {
 } from '@mui/material'
 import { useSelector, useDispatch } from 'react-redux'
 import { updatePersonInfo } from '@store/makeReservationSlice'
+import { openModal } from '@store/displayModalWindowSlice'
 import { useState, useEffect } from 'react'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ModalWindow from '@components/ModalWindow'
 import style from './TravellerInfoForm.module.scss'
 
 const TravellerInfoForm = ({ number }) => {
@@ -27,6 +29,12 @@ const TravellerInfoForm = ({ number }) => {
     return state.bookingClientInfo
   })
 
+  const displayModalWindow = useSelector((state) => {
+    return state.displayModalWindow
+  })
+
+  const { isOpen } = displayModalWindow
+
   const makeReservation = useSelector((state) => {
     return state.makeReservation
   })
@@ -36,7 +44,8 @@ const TravellerInfoForm = ({ number }) => {
   const getClientInfo = () => {
     const { name, phone } = getBookingClientInfo
     if (name === '' || phone === '') {
-      console.log('예약자 정보를 먼저 입력해주세요')
+      setDuplicateClientInfo(false)
+      dispatch(openModal())
     } else {
       console.log('예약자 정보 복사 후 입력')
     }
@@ -53,75 +62,84 @@ const TravellerInfoForm = ({ number }) => {
   }
 
   return (
-    <Accordion
-      defaultExpanded={true}
-      disableGutters={true}
-      sx={{
-        marginTop: '2rem',
-        boxShadow: 'unset',
-        border: '1px solid #ddd',
-        borderRadius: '5px',
-        '&::before': {
-          height: 0,
-        },
-      }}
-    >
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1a-content"
-        sx={{ backgroundColor: '#F2F4FA', marginBottom: '0' }}
+    <>
+      {isOpen && (
+        <ModalWindow
+          text="예약자 정보를 먼저 입력해주세요"
+          primaryBtnText="확인"
+          primaryBtnLink=""
+        />
+      )}
+      <Accordion
+        defaultExpanded={true}
+        disableGutters={true}
+        sx={{
+          marginTop: '2rem',
+          boxShadow: 'unset',
+          border: '1px solid #ddd',
+          borderRadius: '5px',
+          '&::before': {
+            height: 0,
+          },
+        }}
       >
-        <Typography>여행자 정보 {number}</Typography>
-      </AccordionSummary>
-      <AccordionDetails sx={{ padding: '1.6rem' }}>
-        <Box>
-          <Box
-            sx={{
-              display: number === 1 ? 'flex' : 'none',
-              justifyContent: 'flex-end',
-            }}
-          >
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={duplicateClientInfo}
-                  onChange={updateDuplicateClientInfoState}
-                />
-              }
-              label="예약자 정보와 동일"
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          sx={{ backgroundColor: '#F2F4FA', marginBottom: '0' }}
+        >
+          <Typography>여행자 정보 {number}</Typography>
+        </AccordionSummary>
+        <AccordionDetails sx={{ padding: '1.6rem' }}>
+          <Box>
+            <Box
               sx={{
-                margin: '0 0 1rem 0',
-                '& .MuiButtonBase-root': {
-                  padding: '0 0.5rem 0 0',
-                },
+                display: number === 1 ? 'flex' : 'none',
+                justifyContent: 'flex-end',
               }}
-            />
+            >
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={duplicateClientInfo}
+                    onChange={updateDuplicateClientInfoState}
+                  />
+                }
+                label="예약자 정보와 동일"
+                sx={{
+                  margin: '0 0 1rem 0',
+                  '& .MuiButtonBase-root': {
+                    padding: '0 0.5rem 0 0',
+                  },
+                }}
+              />
+            </Box>
+            <div className={style['input-wrapper']}>
+              <div className={style['label']}>이름</div>
+              <TextField
+                name="name"
+                size="small"
+                placeholder="이름을 입력해주세요"
+                value={reservationPersonListDto[number - 1].name}
+                onChange={inputChangeHandler}
+                sx={{ width: '100%' }}
+              />
+            </div>
+            <div className={style['input-wrapper']}>
+              <div className={style['label']}>전화번호</div>
+              <TextField
+                name="phoneNumber"
+                size="small"
+                placeholder="전화번호을 입력해주세요"
+                value={reservationPersonListDto[number - 1].phoneNumber}
+                onChange={inputChangeHandler}
+                sx={{ width: '100%' }}
+              />
+            </div>
           </Box>
-          <div className={style['input-wrapper']}>
-            <div className={style['label']}>이름</div>
-            <TextField
-              name="name"
-              size="small"
-              placeholder="이름을 입력해주세요"
-              value={reservationPersonListDto[number - 1].name}
-              onChange={inputChangeHandler}
-              sx={{ width: '100%' }}
-            />
-          </div>
-          <div className={style['input-wrapper']}>
-            <div className={style['label']}>전화번호</div>
-            <TextField
-              name="phoneNumber"
-              size="small"
-              placeholder="전화번호을 입력해주세요"
-              value={reservationPersonListDto[number - 1].phoneNumber}
-              onChange={inputChangeHandler}
-              sx={{ width: '100%' }}
-            />
-          </div>
-        </Box>
-      </AccordionDetails>
-    </Accordion>
+        </AccordionDetails>
+      </Accordion>
+    </>
   )
 }
 
