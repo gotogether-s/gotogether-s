b41d2d99b6655a2759a40e5f5da1b441f52b5aa9
hyeonah <hyeonah.hello@gmail.com>
2022-12-18 15:25:07 +0900

    #165 rename: Rename favorite to likes for folder, component, style file

--- pages/favorite/index.tsx
+++
@@ -1,7 +0,0 @@
-import style from './Favorite.module.scss'
-
-const Favorite = () => {
-  return <>찜목록페이지는 준비중입니다 😅</>
-}
-
-export default Favorite
---
+++ pages/likes/index.tsx
@@ -0,0 +1,7 @@
+import style from './Likes.module.scss'
+
+const Likes = () => {
+  return <>찜목록페이지는 준비중입니다 😅</>
+}
+
+export default Likes
