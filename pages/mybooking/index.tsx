5bc3144bc6e97f57840c3f82df1c9d9c54db6ef1
hyeonah <hyeonah.hello@gmail.com>
2022-12-28 13:41:04 +0900

    #172 feat: Create Select to choose reservation duration

--- pages/mybooking/index.tsx
+++ pages/mybooking/index.tsx
@@ -1,10 +1,64 @@
+import { styled } from '@mui/material/styles'
+import { Box, Select, MenuItem } from '@mui/material'
+import { useState } from 'react'
 import NavBar from '@components/NavBar'
 import style from './MyBooking.module.scss'
 
+const reservationDurationOptions = [
+  {
+    label: '최근 3개월',
+    value: '90',
+  },
+  {
+    label: '최근 6개월',
+    value: '180',
+  },
+  {
+    label: '최근 1년',
+    value: '365',
+  },
+  {
+    label: '전체',
+    value: 'all',
+  },
+]
+
 const MyBooking = () => {
+  const StyledSection = styled('div')(() => ({
+    backgroundColor: '#fff',
+    padding: '1.6rem',
+    marginBottom: '1.6rem',
+  }))
+
+  const [reservationDuration, setReservationDuration] = useState(
+    reservationDurationOptions[0].value,
+  )
+
+  const handleChange = (e) => {
+    setReservationDuration(e.target.value)
+  }
+
   return (
     <>
       <NavBar link={`/`} title="예약 확인 및 취소" marginBottom="0" />
+      <Box sx={{ backgroundColor: '#F2F4FA' }}>
+        <StyledSection>
+          <Box>
+            <Select
+              fullWidth
+              value={reservationDuration}
+              onChange={handleChange}
+              sx={{ '& legend': { display: 'none' }, '& fieldset': { top: 0 } }}
+            >
+              {reservationDurationOptions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </Box>
+        </StyledSection>
+      </Box>
     </>
   )
 }
