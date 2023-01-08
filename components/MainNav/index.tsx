442164fd4698bd901abb3f56c7fd198351da2828
hyeonah <hyeonah.hello@gmail.com>
2023-01-08 10:22:14 -0500

    #184 design: Replace HTML with MUI, Remove scss file and Update style

--- components/MainNav/index.tsx
+++ components/MainNav/index.tsx
@@ -1,31 +1,76 @@
 import SideBar from '@components/SideBar'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import SearchIcon from '@mui/icons-material/Search'
+import { Box, Typography } from '@mui/material'
+import mainLogo from '@public/main_logo.svg'
+import Image from 'next/image'
 import Link from 'next/link'
-import style from './MainNav.module.scss'
 
 const MainNav = () => {
   return (
-    <nav className={style.nav}>
+    <Box
+      sx={{
+        borderBottom: '1px solid #ddd',
+        padding: '1rem 1.6rem',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
       <SideBar />
-      <Link href="/" className={style['logo']}>
-        <a>로고</a>
+      <Link href="/">
+        <Image
+          src={mainLogo}
+          alt="gotogether logo"
+          width="120%"
+          height="30%"
+          objectFit="contain"
+          style={{ borderRadius: '0.75rem' }}
+        />
       </Link>
-      <div className={style['flex-wrapper']}>
-        <Link href="/likes">
-          <a className={style['flex-wrapper-favorite']}>
-            <FavoriteBorderIcon sx={{ fontSize: 20 }} />
-            <div>찜 목록</div>
-          </a>
-        </Link>
-        <Link href="/product-search">
-          <a>
-            <SearchIcon sx={{ fontSize: 25 }} />
-            <div>검색</div>
-          </a>
-        </Link>
-      </div>
-    </nav>
+      <Box sx={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+        <Box
+          sx={{
+            '&:hover': {
+              cursor: 'pointer',
+            },
+          }}
+        >
+          <Link href="/likes">
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <FavoriteBorderIcon sx={{ fontSize: 20 }} />
+              <Typography sx={{ fontSize: '1.2rem' }}>찜 목록</Typography>
+            </Box>
+          </Link>
+        </Box>
+        <Box
+          sx={{
+            '&:hover': {
+              cursor: 'pointer',
+            },
+          }}
+        >
+          <Link href="/product-search">
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <SearchIcon sx={{ fontSize: 25 }} />
+              <Typography sx={{ fontSize: '1.2rem' }}>검색</Typography>
+            </Box>
+          </Link>
+        </Box>
+      </Box>
+    </Box>
   )
 }
 
