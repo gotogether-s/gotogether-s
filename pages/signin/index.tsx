836df97bdc99afd2f0c0277152dd2cd20ce0d27a
hyeonah <hyeonah.hello@gmail.com>
2023-01-10 12:01:34 -0500

    #188 chore: Organize SignIn page's code

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -11,8 +11,8 @@ import style from './SignIn.module.scss'
 const isEmail = /^([a-z\d.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
 
 const SignIn = () => {
-  const router = useRouter()
   const dispatch = useDispatch()
+  const router = useRouter()
 
   const [requestSignIn] = useRequestSignInMutation()
 
