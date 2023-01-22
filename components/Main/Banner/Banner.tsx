02bbac0d73bbb62285038b2c049bc5acbd8d8d1f
hyeonah <hyeonah.hello@gmail.com>
2023-01-22 17:00:18 -0500

    #238 asset: Replace banner images to png format with new name

--- components/Main/Banner/Banner.tsx
+++ components/Main/Banner/Banner.tsx
@@ -21,21 +21,21 @@ const Banner = () => {
         <SwiperSlide>
           <Link href="/survey">
             <a>
-              <img src="./banner1.jpg" alt="img" className={style.img} />
+              <img src="./banner-ko-1.png" alt="img" className={style.img} />
             </a>
           </Link>
         </SwiperSlide>
         <SwiperSlide>
           <Link href="/product-list/continents?category=괌,사이판,하와이&page=0&sort=">
             <a>
-              <img src="./banner2.jpg" alt="img" className={style.img} />
+              <img src="./banner-ko-2.png" alt="img" className={style.img} />
             </a>
           </Link>
         </SwiperSlide>
         <SwiperSlide>
           <Link href="/product-details/58">
             <a>
-              <img src="./banner3.jpg" alt="img" className={style.img} />
+              <img src="./banner-ko-3.png" alt="img" className={style.img} />
             </a>
           </Link>
         </SwiperSlide>
