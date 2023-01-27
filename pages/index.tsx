13bd6f9d32bf0b3c406f84b4799c24e6725b5cc3
hyeonah <hyeonah.hello@gmail.com>
2023-01-27 17:27:11 -0500

    #257 feat: Add customized page title with translated copy to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
 import HeadInfo from '@components/HeadInfo'
 import Main from '@components/Main/Main'
+import en from '@public/locales/en/home.json'
+import ko from '@public/locales/ko/home.json'
+import { useRouter } from 'next/router'
 
 const Home = () => {
+  const router = useRouter()
+
+  const { locale } = router
+  const translate = locale === 'en' ? en : ko
+
   return (
     <>
-      <HeadInfo title="고투게더 홈페이지" />
+      <HeadInfo title={translate['홈페이지 제목']} />
       <Main />
     </>
   )
